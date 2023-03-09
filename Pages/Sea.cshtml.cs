c697184e5c5c286a1033c9299ff058d582a191d4
VV125 <VV125@CCSLAB526U29>
2023-03-10 07:50:28 +0800

    Add project files.

---
+++ Pages/Sea.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotoAlbum.Pages
+{
+    public class SeaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
